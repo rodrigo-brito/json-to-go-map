6b79294369da09fdb22477737dc0023cdba0778c
Salil Gupta <g.salil@live.in>
2017-10-03 18:25:45 +0530

    Added Copy To ClipBoard Button

--- src/components/converter.jsx
+++ src/components/converter.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import jsonToGoMap from "../lib/jsonToGoMap";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-export default class Converter extends React.Component{
-    constructor(props){
+export default class Converter extends React.Component {
+    constructor(props) {
         super(props)
         this.onChange = this.onChange.bind(this);
         this.state = {
-            result: "" 
+            result: "",
+            value: '',
+            copied: false,
         }
     }
 
-    onChange(event){
+    onChange(event) {
         let result = jsonToGoMap(event.target.value);
         this.setState({
             result
@@ -25,8 +28,14 @@ export default class Converter extends React.Component{
                     className="text-box" />
                 <div className="text-box">
                     <pre>{this.state.result}</pre>
+                    {this.state.result ? <CopyToClipboard text={this.state.result}
+                    onCopy={() => this.setState({ copied: true })}>
+                    <button>Copy to Clipboard</button>
+                </CopyToClipboard> : false}
+                 {this.state.copied ? <span style={{ color: 'red' }}>Copied.</span> : null} 
                 </div>
             </div>
-        )
+
+        );
     }
 }
