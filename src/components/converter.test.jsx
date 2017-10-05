53217d9c7c4bab19a7d81001b8dfc47de407614d
Catalin Vasile <contact@catalin.me>
2017-10-05 12:59:40 +0300

    Added converter onChange unit test.

--- src/components/converter.test.jsx
+++ src/components/converter.test.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Converter from './converter';
+import ReactTestUtils from 'react-dom/test-utils';
+import jsonToGoMap from '../lib/jsonToGoMap.js';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Converter />, div);
+});
+
+it('updates the state on the onChange event', () => {
+  const converter = ReactTestUtils.renderIntoDocument(<Converter />);
+  const textarea = ReactTestUtils.findRenderedDOMComponentWithTag(converter, 'textarea');
+
+  const initialValue = '{ "a" : true }';
+  textarea.value = initialValue;
+  const expectedValue = jsonToGoMap(initialValue);
+
+  ReactTestUtils.Simulate.change(textarea);
+
+  expect(converter.state.result).toBe(expectedValue);
 });
