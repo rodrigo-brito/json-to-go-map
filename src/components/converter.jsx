13352d45efa25bc778754d988ca75b05bb902775
arthurdenner <arthurdenner7@gmail.com>
2017-10-07 23:40:55 -0300

    refactor(converter): Refactoring the Converter component to use ES6 syntax
    I also extended from PureComponent instead of React.Component

--- src/components/converter.jsx
+++ src/components/converter.jsx
@@ -1,40 +1,47 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import jsonToGoMap from "../lib/jsonToGoMap";
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
-export default class Converter extends React.Component {
-    constructor(props) {
-        super(props)
-        this.onChange = this.onChange.bind(this);
-        this.state = {
-            result: "",
-            copied: false,
-        }
-    }
+class Converter extends PureComponent {
+  state = {
+    result: "",
+    copied: false
+  };
 
-    onChange(event) {
-        let result = jsonToGoMap(event.target.value);
-        this.setState({
-            result
-        });
-    }
+  onChange = ({ target: { value } }) => {
+    let result = jsonToGoMap(value);
+    this.setState({
+      result
+    });
+  };
 
-    render() {
-        return (
-            <div className="result-panel">
-                <textarea placeholder="Paste your JSON here"
-                    onChange={this.onChange}
-                    className="text-box" />
-                <div className="text-box">
-                    <pre>{this.state.result}</pre>
-                    {this.state.result ? <CopyToClipboard text={this.state.result}
-                    onCopy={() => this.setState({ copied: true })}>
-                    <button>Copy to Clipboard</button>
-                </CopyToClipboard> : false}
-                 {this.state.copied ? <span style={{ color: 'red' }}>Copied.</span> : null} 
-                </div>
-            </div>
-
-        );
-    }
+  render() {
+    return (
+      <div className="result-panel">
+        <textarea
+          placeholder="Paste your JSON here"
+          onChange={this.onChange}
+          className="text-box"
+        />
+        <div className="text-box">
+          <pre>{this.state.result}</pre>
+          {this.state.result ? (
+            <CopyToClipboard
+              text={this.state.result}
+              onCopy={() => this.setState({ copied: true })}
+            >
+              <button>Copy to Clipboard</button>
+            </CopyToClipboard>
+          ) : (
+            false
+          )}
+          {this.state.copied ? (
+            <span style={{ color: "red" }}>Copied.</span>
+          ) : null}
+        </div>
+      </div>
+    );
+  }
 }
+
+export default Converter;
