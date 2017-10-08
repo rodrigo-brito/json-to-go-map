4f81122825134b9a3cf9776fe76f0b4c35145c95
arthurdenner <arthurdenner7@gmail.com>
2017-10-07 23:33:19 -0300

    refactor(lib): Refactoring all the functions to use ES6 syntax
    I also changed the functions' order to better readibility

--- src/lib/jsonToGoMap.js
+++ src/lib/jsonToGoMap.js
@@ -1,41 +1,52 @@
-export default function convert(jsonString){
-    if (!jsonString) {
-        return  ""
-    }
-    try {
-        const parseObect = JSON.parse(jsonString)
-        return jsonToMap(parseObect);
-    } catch(e) {
-        return e.message
-    }
-}
+const isObject = obj => obj === Object(obj);
 
-function jsonToMap(obj, tabLevel = 1){
-    if (obj === null || obj === undefined){
-        return "nil"
-    } else if (Array.isArray(obj)) {
-        const output = obj.map(key => {
-            return `${getTabs(tabLevel)}${jsonToMap(key, tabLevel + 1)},`
-        }).join("\n")
-        return `[]interface{}{\n${output}\n${getTabs(tabLevel-1)}}`
-    } else if (isObject(obj)) {
-        const values = Object.keys(obj).map(key => {
-            return `${getTabs(tabLevel)}"${key}": ${jsonToMap(obj[key], tabLevel + 1)},`
-        })
-        const output = values.join("\n")
-        return `map[string]interface{}{\n${output}\n${getTabs(tabLevel - 1)}}`
-    }
-    return JSON.stringify(obj)
-}
+const getTabs = numberTabs => {
+  let result = "";
+  for (let i = 0; i < numberTabs; i++) {
+    result += "\t";
+  }
 
-function getTabs(numberTabs) {
-    let result = ""; 
-    for (let i = 0; i < numberTabs; i++){
-        result += "\t";
-    }
-    return result;
-}
+  return result;
+};
 
-function isObject(obj) {
-  return obj === Object(obj);
-}
+const jsonToMap = (obj, tabLevel = 1) => {
+  if (obj === null || obj === undefined) {
+    return "nil";
+  } else if (Array.isArray(obj)) {
+    const output = obj
+      .map(key => {
+        return `${getTabs(tabLevel)}${jsonToMap(key, tabLevel + 1)},`;
+      })
+      .join("\n");
+
+    return `[]interface{}{\n${output}\n${getTabs(tabLevel - 1)}}`;
+  } else if (isObject(obj)) {
+    const values = Object.keys(obj).map(key => {
+      return `${getTabs(tabLevel)}"${key}": ${jsonToMap(
+        obj[key],
+        tabLevel + 1
+      )},`;
+    });
+    const output = values.join("\n");
+
+    return `map[string]interface{}{\n${output}\n${getTabs(tabLevel - 1)}}`;
+  }
+
+  return JSON.stringify(obj);
+};
+
+const convert = jsonString => {
+  if (!jsonString) {
+    return "";
+  }
+
+  try {
+    const parseObect = JSON.parse(jsonString);
+
+    return jsonToMap(parseObect);
+  } catch (e) {
+    return e.message;
+  }
+};
+
+export default convert;
